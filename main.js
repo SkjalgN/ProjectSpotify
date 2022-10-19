0a8c7f32f0f2e68a9bed6cd51a180ddc0f208042
Skjalgn <skjalgn@gmail.com>
2022-10-20 00:53:43 +0200

    Fiksa mye!!!

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function home() {
+    window.location.href = "./startside.html";
+} 
+
+function playlist() {
+    window.location.href = "./spilleliste.html";
+} 
+
+function change_page() {
+    window.location.href = "./spilleliste.html";
+} 
+
+function change_page() {
+    window.location.href = "./spilleliste.html";
+} 
+
+function change_page() {
+    window.location.href = "./spilleliste.html";
+} 
